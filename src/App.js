ef9247e364d8112d21ea503851852f07d2d48fcc
PimKalasinmongkol <pimty.pim@gmail.com>
2024-03-10 17:38:46 +0700

    ver0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import Swal from "sweetalert2";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [money, setMoney] = useState("");
+  const [name, setName] = useState("");
+  const [cardCode, setCardCode] = useState("");
+
+  const handleButtonClick = (amount) => {
+    setMoney(amount.toString());
+  };
+
+  const showSwal = () => {
+    if (!money) {
+      Swal.fire({
+        icon: "error",
+        title: "กรุณากรอกข้อมูล",
+        html: `โปรดกรอกข้อมูลให้ครบถ้วน`,
+        confirmButtonText: "ตกลง",
+      });
+      return;
+    }
+    Swal.fire({
+      title: "บริการเติมเงิน",
+      icon: "question",
+      html: `คุณต้องการเติมเงิน ${money} บาท<br><br>ชื่อ-นามสกุล: ${name}<br>รหัสบัตรของผู้เติม: ${cardCode}`,
+      showCancelButton: true,
+      confirmButtonText: "ตกลง",
+      cancelButtonText: "ยกเลิก",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Perform the actual top-up action
+        Swal.fire("เติมเงินสำเร็จ", `${money} บาท`, "success");
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire("การเติมเงินถูกยกเลิก", "", "error");
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="TopUpPage">
+      <h1>บริการเติมเงิน</h1>
+      <div className="CashNow">
+        <p>200</p>
+      </div>
+      <div className="bigBoxInp">
+        <div className="dataUser">
+          <div className="BoxInput">
+            <p>ชื่อ-นามสกุล</p>
+            <input
+              className="inpName"
+              type="text"
+              value={name}
+              placeholder="ชื่อ-นามสกุล"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="BoxInput">
+            <p>รหัสบัตร</p>
+            <input
+              className="inpCardCode"
+              type="text"
+              value={cardCode}
+              placeholder="รหัสบัตรของผู้เติม"
+              onChange={(e) => setCardCode(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="BoxInput_Cash">
+          <p>กรอกจำนวนเงินที่ต้องการ(บาท)</p>
+          <input
+            type="text"
+            className="inpCash"
+            value={money}
+            placeholder="กรุณาใส่จำนวนเงิน"
+            min="20"
+            max="5000"
+            onChange={(e) => setMoney(e.target.value)}
+          />
+        </div>
+        <div className="chooseCash">
+          <button onClick={() => handleButtonClick(50)}>50</button>
+          <button onClick={() => handleButtonClick(100)}>100</button>
+          <button onClick={() => handleButtonClick(150)}>150</button>
+          <button onClick={() => handleButtonClick(200)}>200</button>
+        </div>
+        <div className="btnYes">
+          <button onClick={showSwal}>ยืนยัน</button>
+        </div>
+      </div>
     </div>
   );
 }
